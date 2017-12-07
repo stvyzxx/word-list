d00c68e8b014cd73a5c127ea8524d8d04e2525bf
stvyzxx <yurasobechko@gmail.com>
2017-12-08 01:39:09 +0200

    list component structure

--- app/home/list/list.component.ts
+++ app/home/list/list.component.ts
@@ -1,26 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
 
+import { ApiService } from '../../shared/services/api.service';
 
 class List {
-    public name: string = null;
-    public words: any[] = [];
-    constructor(
-    ) { 
-    }
+  public name: string = null;
+  public words: any[] = [];
 }
 
 @Component({
-    selector: 'WlList',
-    moduleId: module.id,
-    templateUrl: './list.component.html'
+  selector: 'WlList',
+  moduleId: module.id,
+  templateUrl: './list.component.html'
 })
 export class WlListComponent implements OnInit {
-    list: any;
-    constructor(
-    ) { 
-    }
+  list: List;
 
-    ngOnInit () {
-        this.list = new List();
+  constructor(
+      private routerExtensions: RouterExtensions,
+      private apiService: ApiService
+  ) { }
+
+  ngOnInit () {
+      this.list = new List();
+  }
+
+  addPair () {
+    const pair = {
+      original: null,
+      translation: null
     }
+    this.list.words.push(pair);
+  }
+
+  removePair (index) {
+    this.list.words.splice(index, 1);
+  }
+
+  saveList () {
+
+  }
 }
