949827ade1492cf6a2b5378dd081762db3526e36
stvyzxx <yurasobechko@gmail.com>
2018-01-04 01:12:48 +0200

    started learning page

--- app/home/learning/learning.component.ts
+++ app/home/learning/learning.component.ts
@@ -12,6 +12,12 @@ import { List } from '../models/list/list';
 export class WlLearningComponent implements OnInit {
   list: List;
   busyState: boolean;
+  pair: any;
+  pairsAmount: number;
+  currentNumber: number;
+  answer: string;
+  successfulAnswer: boolean;
+  checkShown: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,6 +26,10 @@ export class WlLearningComponent implements OnInit {
 
   ngOnInit(): void {
     this.setList();
+  }
+
+  get wordsEquality(): boolean {
+    return this.answer.trim().toLowerCase() === this.pair.translation;
   }
 
   setList(): any {
@@ -33,10 +43,37 @@ export class WlLearningComponent implements OnInit {
       path: '/lists/' + id
     }).then(response => {
       this.list = response.value;
+
+      this.pair = this.list.words[0];
+      this.pairsAmount = this.list.words.length;
+      this.currentNumber = this.list.words.indexOf(this.pair) + 1;
+
       this.busyState = false;
     }).catch(e => {
       this.busyState = false;
       console.log(e);
     });
+  }
+
+  checkWord(): void {
+    if (!this.answer) return;
+
+    if (this.currentNumber - 1 === this.list.words.length) {
+      return;
+    }
+    if (this.wordsEquality) {
+      this.successfulAnswer = this.pair.success = true;
+    } else {
+      this.successfulAnswer = this.pair.success = false;
+    }
+    
+    this.checkShown = true;
+    this.answer = '';
+    this.pair = this.list.words[this.currentNumber];
+    this.currentNumber = this.list.words.indexOf(this.pair) + 1;
+
+    setTimeout(() => {
+      this.checkShown = false;
+    }, 2000);
   }
 }
